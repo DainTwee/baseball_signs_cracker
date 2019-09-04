0f1e91efb62da7068fe1aef97cc5e45e9a0f009f
DainTwee <ilan@optusnet.com.au>
2019-09-05 00:05:17 +1000

    Tried AI but it doesn't work yet, better data collection

--- cracker.py
+++ cracker.py
@@ -1,14 +1,24 @@
 # After this add something for once we have it right
+from nltk.classify import NaiveBayesClassifier
 
 def cracker_function():
     no_steal = []
     steal = []
+    model = None
     while True:
         current = get_current_sign()
         if current == "qq":
             break
+        '''if current == "train":
+            steal_feats = [(f, 'steal') for f in steal]
+            no_steal_feats = [(f, 'no steal') for f in no_steal]
+            training_data = steal_feats + no_steal_feats
+            model = NaiveBayesClassifier.train(training_data)'''
         pred_outcome = predict_outcome(current, no_steal, steal)
-        output_pred_outcome(pred_outcome)
+        ai_result = None
+        '''if model:
+            ai_result = get_ai_result(current, model)'''
+        output_pred_outcome(pred_outcome, ai_result)
         true_outcome = get_true_outcome()
         if true_outcome == "steal":
             steal.append(current)
@@ -62,22 +72,34 @@ def guess_prediction(current, steal):
             return ["steal", "50"]
     return ["no steal", "75"]
 
-def output_pred_outcome(pred_outcome):
+def get_ai_result(current, model):
+    return model.classify(current)
+
+def output_pred_outcome(pred_outcome, ai_result):
+    print_statement = ""
     if pred_outcome[0] == "steal":
         if pred_outcome[1] == "100":
-            print("Steal incoming")
+            print_statement += "Basic: Steal incoming"
         else:
-            print("Likely steal")
+            print_statement += "Basic: Likely steal"
     elif pred_outcome[0] == "no steal":
-        print("Probably no steal")
+        print_statement += "Basic: Probably no steal"
     elif pred_outcome[0] == "need data":
-        print("Still need more data")
+        print_statement += "Basic: Still need more data"
     else:
-        print("something happened, investigate this")
+        print_statement += "Basic: something happened, investigate this"
+    '''if ai_result:
+        print(ai_result)'''
+    print(print_statement)
 
 def get_true_outcome():
     print("What actually happened?\tPlease enter 'steal' or 'no steal'.")
-    outcome = input()
+    while True:
+        outcome = input()
+        if outcome in ['steal', 'no steal']:
+            break
+        else:
+            print('Please enter the data correctly')
     return outcome
 
 if __name__ == "__main__":
